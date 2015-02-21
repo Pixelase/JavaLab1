dd66b1e4356ee6594681dd6b84d6b182ab86584e
Alexander Babaj <sustem34@gmail.com>
2015-02-21 14:32:48 +0300

    Code cleanup

--- Program.java
+++ Program.java
@@ -4,70 +4,70 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+public class Program {
+	public static void main(String[] args) throws FileNotFoundException {
+		try {
+			String fileName = args[0];
+			long readFrom = Integer.parseInt(args[1]);
+			long rowsToRead = Integer.parseInt(args[2]);
+			System.out.println(Program.read(fileName, readFrom, rowsToRead));
+		}
 
+		catch (FileNotFoundException e) {
+			System.out.println("Файл не найден\n");
+		}
 
-public class Program
-{	
-    public static void main(String[] args) throws FileNotFoundException
-    {
-    	try{
-    		String fileName = args[0];
-    		int ReadFrom = Integer.parseInt(args[1]);
-    		int RowsToRead = Integer.parseInt(args[2]);    	
-        	System.out.println(Program.read(fileName, ReadFrom, RowsToRead));
-    	}
-    	
-    	catch(Exception e)
-    	{
-    		System.out.println("Что-то пошло не так");
-    	}
-    }
-    
-    private static void exists(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        if (!file.exists()){
-            throw new FileNotFoundException(file.getName());
-        }
-    }
-    
-	public static String read(String fileName, int readFrom, int rowsToRead ) throws FileNotFoundException {
-	    //Этот спец. объект для построения строки
-	    StringBuilder sb = new StringBuilder();
-	 
-	    exists(fileName);
-	 
-	    try {
-	        //Объект для чтения файла в буфер
-	        BufferedReader in = new BufferedReader(new FileReader(fileName));
-	        try {
-	            //В цикле построчно считываем файл
-	        	String s;
-	        	int count = 0;
-	            while ((s = in.readLine()) != null && count - readFrom + 1 != rowsToRead ) {
-	             	count++;
-	            	if(count >= readFrom - 1)
-	            	{
-	                sb.append(s);
-	                sb.append("\n");
-	            	}
-	            }          
-	            
-	        }	        
-	    	catch(Exception ex)
-	    	{
-	    		System.out.println(ex.getMessage());
-	    	}
-	        
-	        finally {
-	            //Также не забываем закрыть файл
-	            in.close();
-	        }
-	    } catch(IOException e) {	    	
-	        throw new RuntimeException(e);
-	    }
-	 
-	    //Возвращаем полученный текст с файла
-	    if (sb.length() == 0) return "Неверные введённые параметры или файл пустой\n";
-	    return sb.toString();
+		catch (Exception e) {
+			System.out.println("Что-то пошло не так:\n"
+					+ e.getMessage().toString());
+		}
+	}
+
+	private static void exists(String fileName) throws FileNotFoundException {
+		File file = new File(fileName);
+		if (!file.exists()) {
+			throw new FileNotFoundException(file.getName());
+		}
+	}
+
+	public static String read(String fileName, long readFrom, long rowsToRead)
+			throws FileNotFoundException {
+
+		StringBuilder sb = new StringBuilder();
+
+		exists(fileName);
+
+		try {
+
+			if (readFrom <= 0 || rowsToRead < 0) {
+				throw new IllegalArgumentException("Введены неверные параметры");
+			}
+
+			// Объект для чтения файла в буфер
+			BufferedReader in = new BufferedReader(new FileReader(fileName));
+			try {
+				// В цикле построчно считываем файл
+				String s;
+				int count = 0;
+				while ((s = in.readLine()) != null
+						&& count - readFrom + 1 != rowsToRead) {
+					count++;
+					if (count >= readFrom - 1) {
+						sb.append(s + '\n');
+					}
+				}
+			}
+
+			finally {
+				in.close();
+			}
+		}
+
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Возвращаем полученный текст с файла
+		return sb.toString();
 	}
 }
